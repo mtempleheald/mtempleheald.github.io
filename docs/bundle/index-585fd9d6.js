d09234d8cf31d6c5bd5e35040d070287c874e151
mtempleheald@hotmail.com <mtempleheald@hotmail.com>
2020-11-09 21:55:29 +0000

    check in result of npm run build

---
+++ docs/bundle/index-585fd9d6.js
@@ -0,0 +1,2 @@
+import{S as s,i as a,s as t,t as e,b as o,n,d as r}from"./main-805550fd.js";function i(s){let a;return{c(){a=e("Blog home page")},m(s,t){o(s,a,t)},p:n,i:n,o:n,d(s){s&&r(a)}}}export default class extends s{constructor(s){super(),a(this,s,null,i,t,{})}}
+//# sourceMappingURL=index-585fd9d6.js.map
