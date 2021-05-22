665cd770087411661eee8ea7cf67e8940f0e009f
Mark Temple-Heald <mtempleheald@hotmail.com>
2021-05-22 18:52:50 +0100

    add topic handling

---
+++ src/routes/topic/index.svelte
@@ -0,0 +1,35 @@
+<script context="module">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ fetch }) {
+		// Use a `limit` querystring parameter to fetch a limited number of posts
+		// e.g. fetch('posts.json?limit=5') for 5 most recent posts
+		const topics = await fetch('/api/topics.json').then((res) => res.json());
+		return {
+			props: {
+				topics
+			}
+		};
+	}
+</script>
+
+<script>
+    export let topics;
+</script>
+
+
+<h1>Topics</h1>
+
+
+{#each topics as topic}
+    <a href="/topic/{topic}">
+		<span>{topic.replace(/\-/g,' ')}</span>
+		<hr/>
+	</a>
+{/each}
+
+
+<style>
+
+</style>
