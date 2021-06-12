f394d5abd2dcb95feec78879c60ab692d2c4c463
Mark Temple-Heald <mtempleheald@hotmail.com>
2021-06-12 12:01:01 +0100

    fix build issue with undefined in path resolver

--- src/routes/api/blogs/[slug].js
+++ src/routes/api/blogs/[slug].js
@@ -3,7 +3,8 @@ import * as fs from 'fs';
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 export async function get({ params }) {
-	const blog = fs.readFileSync('./src/blogs/' + toPath(params.slug), 'utf8');
+	let srcFile = toPath(params.slug);
+	const blog = srcFile ? fs.readFileSync('./src/blogs/' + srcFile, 'utf8') : '';
 	return {
 		body: blog
 	}
