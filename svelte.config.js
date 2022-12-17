42ddae5596f4317b242ace528d5eb0f52c632689
Mark Temple-Heald <mtempleheald@hotmail.com>
2022-12-17 16:23:01 +0000

    upgrade to kit 1.0

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 import md from 'mdsvex';
 import { mdsvexConfig } from './mdsvex.config.js';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	preprocess: [md.mdsvex(mdsvexConfig)],
+	preprocess: [vitePreprocess(), md.mdsvex(mdsvexConfig)],
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
 		}),
 		prerender: {
-			onError: ({ status, path, referrer, referenceType }) => {
+			handleHttpError: ({ status, path, referrer, referenceType }) => {
 				if (path.startsWith('/blog')) throw new Error('Missing a blog page!');
 				console.warn(`${status} ${path}${referrer ? ` (${referenceType} from ${referrer})` : ''}`);
 			}
