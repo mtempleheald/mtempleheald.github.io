1cac1e8c121fc60c891a9667d24e6871ba2ff950
Mark Temple-Heald <mtempleheald@hotmail.com>
2024-11-09 18:36:13 +0000

    upgrade remaining packages and scrap prettier/eslint since I'll be the only author ever and I cba with any more upgrades

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-import md from 'mdsvex';
+import { mdsvex } from 'mdsvex';
 import { mdsvexConfig } from './mdsvex.config.js';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	preprocess: [vitePreprocess(), md.mdsvex(mdsvexConfig)],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
